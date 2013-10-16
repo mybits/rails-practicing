f41b8e7bd693103e2e939c7ecc5775c97af26040
PaulaEs <paulaproxy@gmail.com>
2013-10-17 01:51:23 +0200

    Comment model.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,11 @@
 class Comment
+	include Mongoid::Document  
+	include Mongoid::Timestamps
+	
+	belongs_to :post
+	belongs_to :user
+
+	field :body, type: String
+
+	validates_presence_of :body
 end
